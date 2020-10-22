5467ae2aeefe0aa97d2bf399bc410b2ecc005912
Paul TOURNU <p.tournu@formation-cci12.fr>
2020-10-22 09:05:18 +0200

    BDD

---
+++ xampp/htdocs/Paul/ajoutconcerts.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<title>Test PHP</title>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="">
+		</head>
+		<body>
+		        <h1>Bases de données MySQL</h1>  
+<!-- BDD connect -->
+		        <?php
+		            $servername = 'localhost';
+		            $username = 'root';
+		            $password = '';
+		            $dbname = 'webbd';
+		            
+		            //Connexion à la BDD
+		            $con = mysqli_connect($servername, $username, $password, $dbname);
+
+		            //Vérification de la connexion
+		            if(mysqli_connect_errno($con)){
+		                echo "Erreur de connexion" .mysqli_connect_error();
+		            }
+		            else {
+		            	echo 'Connexion réussie';
+		            }
+
+		        ?>
+				
+				<!-- Liste déroulante pays -->
+				<form action='index.php' method='get'>			
+					<div>
+						<br/>
+						<label>Pays:</label><br/>
+						<select name='pays' id='liste-pays'>
+
+			        <?php
+			            // Affichage des pays
+						$sql ="SELECT * FROM pays";
+						$result=mysqli_query($con, $sql);
+
+						while ( $row = mysqli_fetch_array($result)) {
+							if(isset($_GET['pays'])){
+								if($_GET['pays'] == $row['Nom_Pays']){
+									echo"<option value='".$row['Nom_Pays']."' selected='selected' >".$row['Nom_Pays']."</option>";
+								}
+								else{
+									echo"<option value='".$row['Nom_Pays']."'>".$row['Nom_Pays']."</option>";
+								}
+							}else{
+								echo"<option value='".$row['Nom_Pays']."'>".$row['Nom_Pays']."</option>";
+							}
+						//echo "<br/>" . $row['Nom_Pays']   ;
+						}
+
+					?>
+			    		</select>
+					</div>
+					<input type="submit" value="Valider"/>
+				</form>
+
+				<!-- Liste déroulante villes -->		
+				<div>
+					<br/>
+					<label>Villes:</label><br/>
+					<select name='villes' id='liste-villes'>
+						<?php
+							if(isset($_GET['pays'])){
+								$sql ="SELECT * FROM ville V WHERE V.id_pays = (SELECT P.id_pays FROM pays P WHERE nom_pays = '".$_GET['pays']."')";
+								echo $sql."</BR>";
+								$result=mysqli_query($con, $sql);
+								while ( $row = mysqli_fetch_array($result)) {
+									echo "<option value ='".$row['Nom_Ville']."'> " .$row['Nom_Ville']."</option>";
+								}
+								unset($_GET['pays']);
+							}
+						?>
+			    	</select>
+				</div>
+
+
+
+<!-- BDD Disconnect -->
+				<?php
+					mysqli_free_result($result);
+					mysqli_close($con);
+		        ?>
+		        
+
+		</body>
+</html> 
