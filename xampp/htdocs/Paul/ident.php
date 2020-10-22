40af8913ed5f1a6ef9591230b07f4457b9f0fe74
Paul TOURNU <p.tournu@formation-cci12.fr>
2020-10-22 10:14:57 +0200

    Formulaire

---
+++ xampp/htdocs/Paul/ident.php
@@ -0,0 +1,13 @@
+!DOCTYPE html>
+<html>
+		<head>
+			<title>Test PHP</title>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="">
+			<titleC></title>
+			<meta name="Author" content="BUSQUET_TOURNU" />
+			<meta name="Keywords" content="ConcertAll" />
+			<meta name="Description" content="Ajout Concerts" />
+		</head>
+
+		<body>Créer un compte</h1>  
