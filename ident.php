331eff14accf815ba51c220775c8a38853ac2706
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2020-11-08 14:29:12 +0100

    ident commit

--- ident.php
+++ ident.php
@@ -1,47 +1,40 @@
 <?php
-		      
-	$servername = 'localhost';
-	$username = 'root';
-	$password = '';
-	$dbname = 'webbd';
 
-	//Connexion à la BDD
-	$con = mysqli_connect($servername, $username, $password, $dbname);
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'webbd';
 
-	//Vérification de la connexion
-	if(mysqli_connect_errno($con)){
-	echo "Erreur de connexion" .mysqli_connect_error();
-	}
-	else {
-	echo 'Connexion réussie';
-	}
+    //Connexion à la BDD
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+
+    //Vérification de la connexion
+    if(mysqli_connect_errno($con)){
+    echo "Erreur de connexion" .mysqli_connect_error();
+    }
+    else {
+    echo 'Connexion réussie';
+    }
 
 if (isset($_POST['inscription']))
 {
-	echo "bonjour";
-	$pseudo = $_POST['pseudo'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$cpassword = $_POST['cpassword'];
+    echo "bonjour";
+    $pseudo = $_POST['pseudo'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
 
-	if (strcmp($password, $cpassword) == 0)
-	{
-			$sql = "SELECT pseudo FROM utilisateur WHERE pseudo = $pseudo";
-			$result = mysqli_query($con, $sql);
-			$row = mysqli_fetch_assoc($result);
-			if ($pseudo == $row['pseudo'])
-			{
-				echo ' Pseudo déja utilisé';
-			}
-			else 
-			{
-			if ($result == TRUE)
-			{
-				$sql = "INSERT INTO utilisateur (pseudo, email, password) VALUES ('$pseudo', '$email', '$password')";
-				mysqli_query($con, $sql);
-			}
-			else{echo'Erreur, l\'utilisateur existe deja';}
-		}
-	}
-	else{echo'Confirmation du mot de passe non valide, réessayer';}
+    if (strcmp($password, $cpassword) == 0)
+    {
+            $sql = "SELECT pseudo FROM utilisateur WHERE pseudo = $pseudo";
+            $result = mysqli_query($con, $sql);
+            if ($result == FALSE)
+            {
+                $sql = "INSERT INTO utilisateur (pseudo, email, password) VALUES ('$pseudo', '$email', '$password')";
+                mysqli_query($con, $sql);
+                header('Location: ./connexion.php');
+            }
+            else{echo'Erreur, l'utilisateur existe deja';}
+        }
+    else{echo'Confirmation du mot de passe non valide, réessayer';}
 }
