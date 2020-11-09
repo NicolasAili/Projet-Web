8fcebb21508d81f42fb97bb97e04c2160d84a68b
paultournu <58809314+paultournu@users.noreply.github.com>
2020-11-09 04:14:41 +0100

    accueil

--- accueil.php
+++ accueil.php
@@ -15,6 +15,32 @@
 		<?php include('header.php'); ?>
 	</header>
 	<body>
+		<div class="main">
+		<h1> Objectifs du site </h1>
+		<p> L'objectif de notre site web est de trouver des concerts de hiphop facilement. 
+			Pour cela, chaque personne qui accède au site va pouvoir ajouter un concert, peut importe l'endroit dans le monde, qu'il soit utilisateur ou non. 
+			De plus, notre site propose un état de connexion de façon à pouvoir supprimer des concerts qui ont mal était renseigné, nous faisons confiance à notre communauté. 
+		</p>
+		<br/>
+		<h1>Fonctionnement du site</h1>
+		<p> <h2>Informations sur un concert</h2>
+			Une fois sur la page d'accueil, nous avons plusieurs onglet qui corresponde aux concerts (artiste, pays, ville). Ces onglets vont nous informer des concerts qu'il y a en fonction de la page qu'on choisi. </p>
+
+			<h2>Consulter tous les concerts</h2>
+			<p>Nous pouvons aussi accéder à la pas "Tous les concerts", comme son nom l'indique, cette page référence tous les concerts qui ont été ajouté par notre communauté. </p>
+
+			<h2>Comment ajouter un concert</h2>
+			<p>Pour ajouter un concert il faut cliquer sur le bouton ajouter concert, et celui si va vous rediriger vers un formulaire et vous aurez plus qu'a rensigné les informations necessaire du concert pour l'ajouter.</p>
+
+			<h2>Inscription et connexion au site</h2>
+			<p> Pour s'inscrire c'est simple, vous avez juste à cliquer sur le bouton inscription et remplir le formulaire demandé en respectant les paramètres (exemple : le mot de passe doit contenir au moins 8 caractères). Une fois inscri vous allez être redirigé vers la page de connexion et vous aurez juste à saisir votre pseudo et votre mot de passe.
+			Si vous êtes déjà inscri, cliquez sur le bouton connexion et entré vos identifiants. </p>
+
+			<h2> A savoir </h2>
+			<p> Seul une personne connecté pourra supprimer des concerts qui ont était annulé, repporté, ou seulement mal référencé.</p>
+
+		</div>
+
 	</body>
 		<!--
 		<div class="main">
