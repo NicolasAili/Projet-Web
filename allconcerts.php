8e5104667be4e01571eb4c55f4b117d60e03e805
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2020-11-09 02:11:12 +0100

    commit

--- allconcerts.php
+++ allconcerts.php
@@ -7,7 +7,7 @@
 		<title>Tous les concerts</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/header.css" media="screen" />	
-		<link rel="stylesheet" type="text/css" href="css/formulaire.css" media="screen" />	
+		<link rel="stylesheet" type="text/css" href="css/body/allconcerts.css" media="screen" />	
 		<?php include("salle.php"); // on appelle le fichier?>
 		<meta name="Author" content="BUSQUET_TOURNU" />
 		<meta name="Keywords" content="ConcertAll" />
@@ -43,25 +43,40 @@
 			$admin = 'administateur';
 			//echo $_SESSION['pseudo'];
 			//echo $admin;
-			while($row = mysqli_fetch_array($result)) {
-				?> <div id="artiste"> <?php echo $row['artiste'] ?> </div>  <div id="date"> <?php echo  $row['date'] ?> </div>  <div id="heure"> <?php echo $row['heure'] ?> </div>  <div id="pays"> <?php echo  $row['pays'] ?> </div> <div id="ville"> <?php echo $row['ville'] ?> </div>  <div id="salle"> <?php echo  $row['salle'] ?> </div> <div id="adresse"> <?php echo $row['adresse'] ?> </div>  <div id="cp"> <?php echo  $row['cp'] ?> </div>
-				?>
+			?>
+			<div id="concertsall">
 				<?php
-				/*echo "success1";
-				if ($_SESSION['pseudo'] == $admin)
-					 {
-					 	echo "success2";
-					 	?>
-							<input  type="submit" value="Supprimer le concert" name="supprimer">
-							</br>
-					 	<?php
-					 }
-				else
-				{
-					echo "success3";
-				}*/
-			}
-		?>
+					while($row = mysqli_fetch_array($result)) 
+					{
+						?> 
+						<div id="inwhile"> 
+							<div id="artiste"> <?php echo $row['artiste'] ?> </div>  
+							<div id="date"> <?php echo  $row['date'] ?> </div>  
+							<div id="heure"> <?php echo $row['heure'] ?> </div>  
+							<div id="pays"> <?php echo  $row['pays'] ?> </div> 
+							<div id="ville"> <?php echo $row['ville'] ?> </div>  
+							<div id="salle"> <?php echo  $row['salle'] ?> </div> 
+							<div id="adresse"> <?php echo $row['adresse'] ?> </div>  
+							<div id="cp"> <?php echo  $row['cp'] ?> </div>
+							?>
+						</div>
+						<?php
+						/*echo "success1";
+						if ($_SESSION['pseudo'] == $admin)
+							 {
+							 	echo "success2";
+							 	?>
+									<input  type="submit" value="Supprimer le concert" name="supprimer">
+									</br>
+							 	<?php
+							 }
+						else
+						{
+							echo "success3";
+						}*/
+					}
+				?>
+			</div>
 	</body>
 	<script type="text/javascript" src="./js/scrollnav.js"></script> 
 </html>
